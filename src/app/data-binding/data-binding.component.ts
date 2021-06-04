c6ef4d8b0a3d14a80d1dce3613adb2d929274990
matheus gobo <matheus.gobo@maxiconsystems.com.br>
2021-06-04 18:24:15 -0300

    Commit 2

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -10,13 +10,34 @@ export class DataBindingComponent implements OnInit {
   url: string = 'http://loiane.com';
   urlImagem: string = 'http://lorempixel.com.br/500/400/?2';
 
+  valorAtual: string = '';
+
+  valorSalvo?:string;
 
   cursoAngular: boolean = true;
+
+  isMouseOver:boolean = false;
 
   getValor(){
     return 1;
   }
 
+  botaoClicado(){
+    alert('Botão clicado');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    console.log((<HTMLInputElement>evento.target).value);
+    this.valorAtual = (<HTMLInputElement>evento.target).value
+  }
+
+  salvarValor(valor?: any) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
   getCurtirCurso(){
     return true;
   }
